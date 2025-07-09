b80361869e72eb22a562bb5bbaf78c70db07bd5a
finne-admin <admin@finne.app>
2025-07-09 12:44:17 +0200

    duration changes

--- components/milestones/PuntoVolador.tsx
+++ components/milestones/PuntoVolador.tsx
@@ -36,7 +36,7 @@ export function PuntoVolador({ from, to }: PuntoVoladorProps) {
         opacity: [1, 0.8, 0],
       }}
       transition={{
-        duration: 1.4,
+        duration: 2,
         ease: 'easeInOut',
       }}
     />
