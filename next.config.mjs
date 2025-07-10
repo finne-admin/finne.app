22da2ec4a84ab6315b0a859703796257baada7af
finne-admin <admin@finne.app>
2025-07-10 11:22:14 +0200

    config urls

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,9 @@ import nextPwa from 'next-pwa'
 
 /** @type {import('next').NextConfig} */
 const withPWA = nextPwa({
-    // Where to generate the service worker
     dest: 'public',
-
-    // Automatically register SW on client
     register: true,
-
-    // Prompt new service worker to take over immediately
     skipWaiting: true,
-
-    // (Optional) runtimeCaching or additional Workbox config here
-    // runtimeCaching: [...]
 })
 
 /** @type {import('next').NextConfig} */
@@ -24,14 +16,18 @@ const nextConfig = {
         appDir: true
     },
     images: {
-        domains: ['embed-ssl.wistia.com', 'embed.wistia.com'],
+        domains: [
+            'embed-ssl.wistia.com',
+            'embed.wistia.com',
+            'cgpqlasmzpabwrubvhyl.supabase.co' // añadido dominio de Supabase
+        ],
     },
     async redirects() {
         return [
             {
                 source: "/",
                 destination: "/notification",
-                permanent: true, // Use true for 308 status code (permanent redirect), false for 307 (temporary)
+                permanent: true,
             },
         ]
     }
