2db3922d9acf4222671ec0dd1b7825e16298dc69
finne-admin <admin@finne.app>
2025-07-15 19:39:34 +0200

    use client fix

--- components/milestones/TodosLosLogros.tsx
+++ components/milestones/TodosLosLogros.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useEffect, useState } from 'react'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { AchievementCard, Logro } from '@/components/milestones/AchievementCard'
