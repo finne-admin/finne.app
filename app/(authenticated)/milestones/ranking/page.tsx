1b7fefbd716f42935e55bc84bbad6e5510065b1e
finne-admin <admin@finne.app>
2025-09-09 18:09:45 +0200

    style fix

--- app/(authenticated)/milestones/ranking/page.tsx
+++ app/(authenticated)/milestones/ranking/page.tsx
@@ -3,19 +3,19 @@ import { RankingUsuarios } from '@/components/milestones/RankingUsuarios'
 export default function RankingPage() {
   return (
     <div className="px-6 py-8">
-      <h2
-        className="
-          text-2xl md:text-3xl font-extrabold tracking-wide mb-8 text-center
-          text-gradient animate-gradient-fast hover:[animation-duration:3s]
-          motion-reduce:animate-none bg-texture-emerald breath select-none cursor-default
-        "
-        style={{
-          ['--tw-gradient-image' as any]:
-            'linear-gradient(90deg, #059669, #10b981, #047857)', // tus verdes
-        }}
-      >
-        Temporada Post Verano — 1 septiembre — 30 noviembre
-      </h2>
+    <h2
+      className="
+        text-2xl md:text-3xl font-extrabold tracking-wide mb-8 text-center
+        text-gradient bg-melting-emerald-noise animate-melting breath
+        select-none cursor-default
+      "
+      style={{
+        ['--tw-gradient-image' as any]:
+          'linear-gradient(90deg, #059669, #10b981, #047857)',
+      }}
+    >
+      Temporada Post Verano — 1 septiembre — 30 noviembre
+    </h2>
 
 
       <RankingUsuarios />
